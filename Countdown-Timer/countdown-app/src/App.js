e80c05ef6fabea21fd82b7661b109991ad53353c
techwebdev <63657518+techwebdev@users.noreply.github.com>
2020-10-10 13:25:50 +0530

    timer logic

--- Countdown-Timer/countdown-app/src/App.js
+++ Countdown-Timer/countdown-app/src/App.js
@@ -1,26 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
+import "./App.css";
+import TimerLogic from "./TimerLogic";
 function App() {
+  const date = "1 Jan 2021";
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* Make desing in TimerLogic */}
+      <TimerLogic date={date} />;
     </div>
   );
 }
-
 export default App;
